c34fcc04451ed96685b13c60549476d4cb26bcad
Ericky Thierry <contato@ericky.ga>
2023-03-18 18:56:46 -0300

    commit

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# API de Transcrição de Áudio para Texto
+
+Uma API simples criada com Python Flask e a biblioteca SpeechRecognition para transcrever arquivos de áudio em texto.
+
+## Uso
+
+### Requisitos
+
+Antes de usar a API, certifique-se de ter instalado as dependências do projeto usando o seguinte comando:
+
+```pip install -r requirements.txt```
+
+
+### Endpoint
+
+A API possui um único endpoint em `/transcrever`, que pode ser usado para enviar arquivos de áudio para transcrição.
+
+Para enviar um arquivo de áudio para transcrição, faça uma solicitação HTTP POST para o endpoint `/transcrever` com o arquivo de áudio como dados do formulário multipart.
+
+Por exemplo, usando o comando `curl` no terminal:
+
+```
+curl -X POST -F 'audio=@/path/to/audio.wav' http://localhost:5000/transcrever
+```
+
+
+Isso enviará um arquivo de áudio `audio.wav` localizado em `/path/to/` para o endpoint `/transcrever` da API. Se o arquivo for um arquivo WAV válido, a API transcreverá o áudio em texto e retornará a transcrição como uma resposta HTTP 200 OK. Se o arquivo enviado não for um arquivo WAV válido, a API retornará um JSON com uma mensagem de erro indicando que apenas arquivos WAV são permitidos.
+
+## Contribuindo
+
+Sinta-se à vontade para copiar ou contribuir com a API criando pull requests.
---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import speech_recognition as sr
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return '<center>[POST] /transcrever with "audio" form file (only wav)</center>'
+
+@app.route('/transcrever', methods=['POST'])
+def transcrever():
+    if 'audio' not in request.files:
+        return 'Nenhum arquivo de áudio enviado', 400
+
+    audio_file = request.files['audio']
+    if not audio_file:
+        return 'Arquivo de áudio inválido', 400
+    
+    # verifique se o arquivo é um arquivo WAV
+    if audio_file.content_type not in ['audio/wav', 'audio/wave', 'audio/x-wav']:
+        return {'erro': 'Apenas arquivos WAV são permitidos'}, 400
+
+    # inicializa o reconhecedor de fala
+    recognizer = sr.Recognizer()
+    
+    # abre o arquivo de áudio com o SpeechRecognition
+    with sr.AudioFile(audio_file) as source:
+        audio_data = recognizer.record(source)
+    
+    try:
+        # utiliza a função recognize_google para transcrever o áudio em texto
+        transcribed_text = recognizer.recognize_google(audio_data, language='pt-BR')
+        return transcribed_text, 200
+    except sr.UnknownValueError:
+        return 'Não foi possível reconhecer o áudio', 400
+    except sr.RequestError:
+        return 'Erro ao se comunicar com o serviço de reconhecimento de fala', 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
