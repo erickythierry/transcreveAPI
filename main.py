6058e02395d92b33aac7f652ab2d458bfd148c97
Ericky Thierry <erickythierry@gmail.com>
2025-03-19 11:11:08 -0300

    correção para evitar que a api bloqueie todos os acessos caso o env ALLOWED_IPS não tenha nada definido. agora a api permite qualquer acesso caso nao tenham sido definidos ips especificos

--- main.py
+++ main.py
@@ -18,59 +18,74 @@ logging.basicConfig(
 )
 
 # Pegar IPs permitidos da variável de ambiente e remover espaços em branco
-ALLOWED_IPS = {ip.strip() for ip in os.getenv('ALLOWED_IPS', '').split(',') if ip.strip()}
+raw_ips = os.getenv('ALLOWED_IPS', '').strip()
+ALLOWED_IPS = {ip.strip() for ip in raw_ips.split(',') if ip.strip(
+)} if raw_ips else None  # None significa "qualquer IP é permitido"
 
-logging.info(f"IPs permitidos: {ALLOWED_IPS}")
+logging.info(f"IPs permitidos: {ALLOWED_IPS if ALLOWED_IPS else 'Todos'}")
+
 
 def check_ip(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # Obtém o IP real, considerando headers de proxy
-        request_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        if not ALLOWED_IPS or request_ip not in ALLOWED_IPS:
-            logging.warning(f"Tentativa de acesso não autorizada do IP: {request_ip}")
+        request_ip = request.headers.get(
+            'X-Forwarded-For', request.remote_addr)
+
+        # Se ALLOWED_IPS for None, permite qualquer IP
+        if ALLOWED_IPS is not None and request_ip not in ALLOWED_IPS:
+            logging.warning(
+                f"Tentativa de acesso não autorizada do IP: {request_ip}")
             return 'Acesso não autorizado', 403
+
         logging.info(f"Acesso autorizado do IP: {request_ip}")
         return f(*args, **kwargs)
+
     return decorated_function
+
 
 @app.before_request
 def log_request_info():
     logging.info(f"Request IP: {request.remote_addr}")
     logging.info(f"Headers: {dict(request.headers)}")
 
-    
+
 @app.route('/', methods=['GET'])
 @check_ip
 def home():
     return '<center><h1>[POST] /transcrever with "audio" form file (wav, ogg, mp3)</h1></center>'
+
 
 @app.route('/transcrever', methods=['POST'])
 @check_ip
 def transcrever():
     request_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     request_ip = request.remote_addr
-    
+
     logging.info(f"Requisição recebida do IP: {request_ip}")
-    
+
     if 'audio' not in request.files:
-        logging.error(f"{request_time} - Nenhum arquivo de áudio enviado - IP: {request_ip}")
+        logging.error(
+            f"{request_time} - Nenhum arquivo de áudio enviado - IP: {request_ip}")
         return 'Nenhum arquivo de áudio enviado', 400
 
     audio_file = request.files['audio']
     if not audio_file:
-        logging.error(f"{request_time} - Arquivo de áudio inválido - IP: {request_ip}")
+        logging.error(
+            f"{request_time} - Arquivo de áudio inválido - IP: {request_ip}")
         return 'Arquivo de áudio inválido', 400
 
     content_type = audio_file.content_type
     if content_type not in ['audio/wav', 'audio/wave', 'audio/x-wav', 'audio/ogg', 'audio/mp3']:
-        logging.error(f"{request_time} - Tipo de arquivo não suportado: {content_type} - IP: {request_ip}")
+        logging.error(
+            f"{request_time} - Tipo de arquivo não suportado: {content_type} - IP: {request_ip}")
         return {'erro': 'Apenas arquivos WAV, OGG e MP3 são permitidos'}, 400
 
     try:
         if content_type in ['audio/ogg', 'audio/mp3']:
-            audio = AudioSegment.from_file(io.BytesIO(audio_file.read()), format=content_type.split('/')[1])
-            audio = audio.set_frame_rate(16000).set_channels(1)  # Ajuste a taxa de amostragem e canais
+            audio = AudioSegment.from_file(io.BytesIO(
+                audio_file.read()), format=content_type.split('/')[1])
+            audio = audio.set_frame_rate(16000).set_channels(
+                1)  # Ajuste a taxa de amostragem e canais
             wav_io = io.BytesIO()
             audio.export(wav_io, format='wav')
             wav_io.seek(0)
@@ -79,18 +94,23 @@ def transcrever():
         recognizer = sr.Recognizer()
         with sr.AudioFile(audio_file) as source:
             audio_data = recognizer.record(source)
-        
-        transcribed_text = recognizer.recognize_google(audio_data, language='pt-BR')
-        
-        logging.info(f"{request_time} - Transcrição bem-sucedida: {transcribed_text} - IP: {request_ip}")
+
+        transcribed_text = recognizer.recognize_google(
+            audio_data, language='pt-BR')
+
+        logging.info(
+            f"{request_time} - Transcrição bem-sucedida: {transcribed_text} - IP: {request_ip}")
         return transcribed_text, 200
 
     except sr.UnknownValueError:
-        logging.error(f"{request_time} - Não foi possível reconhecer o áudio - IP: {request_ip}")
+        logging.error(
+            f"{request_time} - Não foi possível reconhecer o áudio - IP: {request_ip}")
         return 'Não foi possível reconhecer o áudio', 400
     except sr.RequestError as e:
-        logging.error(f"{request_time} - Erro ao se comunicar com o serviço de reconhecimento de fala: {e} - IP: {request_ip}")
+        logging.error(
+            f"{request_time} - Erro ao se comunicar com o serviço de reconhecimento de fala: {e} - IP: {request_ip}")
         return 'Erro ao se comunicar com o serviço de reconhecimento de fala', 500
     except Exception as e:
-        logging.error(f"{request_time} - Erro inesperado: {e} - IP: {request_ip}")
+        logging.error(
+            f"{request_time} - Erro inesperado: {e} - IP: {request_ip}")
         return 'Erro interno no servidor', 500
